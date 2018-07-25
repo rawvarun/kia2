4853015a7534dd1fc969c2dfeda591ab559f6d0c
shashmca <shashank.etw@gmail.com>
2018-07-25 12:54:28 +0530

    Updated inventory listing

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import '../css/import.scss';
 import './vendor/slick.min.js';
+import './vendor/multirange.js';
 import './slider.js';
+
+$(function() {
+  $('[type=range]').on('change', function() {
+    var input = document.querySelector('.original[type=range]'),
+      selectedRange = '';
+    if (input.valueLow && input.valueHigh) {
+      selectedRange = '$' + (input.valueLow * 1000) + '-' + '$' + (input.valueHigh * 1000);
+      $('.js-range-val').text(selectedRange);
+    }
+  })
+})
