e2979a6f07a305a029f73668d12a9d505f19709e
Manil Jain <mjain43@sapient.com>
2018-07-26 11:53:20 +0530

    header animation

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ import './vendor/slick.min.js';
 import './vendor/multirange.js';
 import './slider.js';
 
-$(function() {
-  $('[type=range]').on('change', function() {
+$(function () {
+  $('[type=range]').on('change', function () {
     var input = document.querySelector('.original[type=range]'),
       selectedRange = '';
     if (input.valueLow && input.valueHigh) {
@@ -15,7 +15,25 @@ $(function() {
     }
   });
 
-  $('[data-toggle]').on('click', function() {
+  $('[data-toggle]').on('click', function () {
     $(this).closest('.list-item').toggleClass('expanded-list')
-  })
+  });
+
+  $('#toggle-nav').on('click', function () {
+    if (this.checked == true) {
+      $("header").addClass("expanded");
+    } else {
+      //console.log('a');
+      $("header").removeClass("expanded");
+    }
+  });
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $("header").addClass("theme");
+    } else {
+      $("header").removeClass("theme");
+    }
+  });
 })
