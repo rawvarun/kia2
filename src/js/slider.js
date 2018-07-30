f140e0c6420f84ddf175898d9c0889f63b74c076
shashmca <shashank.etw@gmail.com>
2018-07-30 10:49:45 +0530

    Applied theming

--- src/js/slider.js
+++ src/js/slider.js
@@ -14,34 +14,16 @@ $(function() {
   });
   $(".single-item1").slick({
     infinite: true,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 3,
     arrows: false,
-    responsive: [
-      // {
-      //   breakpoint: 1024,
-      //   settings: {
-      //     slidesToShow: 3,
-      //     slidesToScroll: 3,
-      //     infinite: true,
-      //     dots: true
-      //   }
-      // },
-      // {
-      //   breakpoint: 600,
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll: 2
-      //   }
-      // },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
-    ]
+    }]
   });
   $('.carousel4').slick({
     centerMode: true,
@@ -49,8 +31,7 @@ $(function() {
     infinite: false,
     slidesToShow: 2,
     arrows: true,
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 768,
         settings: {
           centerMode: true,
